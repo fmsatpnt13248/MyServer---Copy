8e56cc3c566f815ef613f20a5be72777eaec4b80
Desys Boiko <denis2005beliy@gmail.com>
2024-07-13 21:03:45 +0200

    second commit

--- includes/animalFunctions.php
+++ includes/animalFunctions.php
@@ -3,6 +3,7 @@
     //Add a class
     include("animals.php");
 
+	//Decide which Animal object is used
 	$animal_definition = function ( $type ) {
 
 		if( $type == 1 ) {
@@ -21,18 +22,15 @@
 
 	};
 
-    //Doesn't work
+    //Doesn't work, variables should have been assigned in the function arguments
 	function animals_do( $type, $sentence) {
 
-		echo $$type->name . ' ', $$type->actions[rand(0, 2)] . ': ', $sentence;
+		echo $$type->name . ' ', $$type->actions[rand(0, 2)] . ': ', $sentence . "<br>"; //The same as the second
 
 	}
-
 	animals_do( type: $animal_definition( 1 ), sentence: "Dogs suck" );
 
-    echo "<br>";
-
-    //Works
+    //Works, manual var assignment
     $type = $animal_definition( 1 );
     $sentence = "Dogs suck";
-	echo $$type->name . ' ', $$type->actions[rand(0, 2)] . ': ', $sentence;
+	echo $$type->name . ' ', $$type->actions[rand(0, 2)] . ': ', $sentence . "<br>"; //The same as the first
